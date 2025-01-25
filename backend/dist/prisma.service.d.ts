0c88c5de7b52fa9a13d13689e5b1f380d2ceecb8
artem <arta90633@gmail.com>
2025-01-25 21:45:57 +0500

    removed build files

--- backend/dist/prisma.service.d.ts
+++
@@ -1,5 +0,0 @@
-import { OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from "prisma/generated/client";
-export declare class PrismaService extends PrismaClient implements OnModuleInit {
-    onModuleInit(): Promise<void>;
-}
