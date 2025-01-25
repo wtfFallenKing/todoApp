e95943838f3c7ec4963c332cfc160411ce5751bb
artem <arta90633@gmail.com>
2025-01-25 20:28:06 +0500

    backend fixed

--- backend/src/auth/decorators/user.decorator.ts
+++ backend/src/auth/decorators/user.decorator.ts
@@ -1,5 +1,5 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User } from 'prisma/generated/client';
+import { User } from '@prisma/client';
 
 export const CurrentUser = createParamDecorator((data: keyof User, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
